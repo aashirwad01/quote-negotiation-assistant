77b53f6ecb4792edb950b504f78c22f0b8e96141
aashirwad01 <aashirwadkumar159@gmail.com>
2025-09-01 19:59:22 +0530

    Initialized backend structure with all the dtos, entities and package declartion

---
+++ backend/src/main/java/com/quote/QuoteNegotiationApplication.java
@@ -0,0 +1,11 @@
+package com.quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteNegotiationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteNegotiationApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/quote/llm/LLMEngine.java
@@ -0,0 +1,1 @@
+package com.quote.llm;
